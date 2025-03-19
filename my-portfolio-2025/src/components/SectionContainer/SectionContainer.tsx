3d6f3c7264b44d562689c256a6deb4d0dce598e3
CamiloMarin <camilomv1234@gmail.com>
2025-03-18 23:34:21 -0500

    Cree mi primera sección con props

---
+++ my-portfolio-2025/src/components/SectionContainer/SectionContainer.tsx
@@ -0,0 +1,12 @@
+export function SectionContainer(props: { className: string; height: number; id: string; children: any; }){
+    let className = props.className;
+    let height = props.height;
+    let id = props.id;
+    let children = props.children;
+    return(
+        <section id={id} className={className} style={{ height: `${height}px` }}>
+            {children}
+        </section>
+    )
+}
+
