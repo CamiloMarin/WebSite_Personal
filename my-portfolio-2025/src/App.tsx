a7aae958ba1af71e4e5919dc7c47a11297c97c0d
CamiloMarin <camilomv1234@gmail.com>
2025-06-18 17:52:44 -0500

    Cambios varios

--- my-portfolio-2025/src/App.tsx
+++ my-portfolio-2025/src/App.tsx
@@ -1,18 +1,29 @@
-import './App.css'
-import { SectionContainer } from './components/SectionContainer/SectionContainer';
-
+import "./App.css";
+import "./components/SectionContainer/SectionContainer.css";
+import { SectionContainer } from "./components/SectionContainer/SectionContainer";
 
 function App() {
   return (
     <>
       <h1>Vite + React</h1>
 
-      <SectionContainer id='section1' className='flex items-center justify-center' height={300}>
-          <h2>Primera sección</h2>
+      <SectionContainer
+        id="section1"
+        className="flex items-center justify-center"
+        height={300}
+      >
+        <h2>Primera sección</h2>
       </SectionContainer>
 
+      <SectionContainer
+        id="section2"
+        className="flex items-center justify-center red_background totalWidth"
+        height={500}
+      >
+       <h2>Segunda sección</h2>
+      </SectionContainer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
